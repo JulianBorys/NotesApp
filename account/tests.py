fb15b763fc0321c13e434d2301ecb80ec7fa76ab
DamianKluczyn <damiankluczynski00@gmail.com>
2025-01-21 01:47:38 +0100

    Dodano testy jednostkowe

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class AccountViewsTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='password123')
+        self.login_url = reverse('login')
+        self.register_url = reverse('register')
+        self.dashboard_url = reverse('dashboard')
+        self.settings_url = reverse('user_settings')
+
+    def test_user_login_invalid_credentials(self):
+        response = self.client.post(self.login_url, {'username': 'wronguser', 'password': 'wrongpass'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Wprowadź poprawne wartości pól')
+
+    def test_user_login_valid_credentials(self):
+        response = self.client.post(self.login_url, {'username': 'testuser', 'password': 'password123'})
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, self.dashboard_url)
+
+    def test_user_register_valid_data(self):
+        response = self.client.post(self.register_url, {
+            'username': 'newuser',
+            'email': 'newuser@example.com',
+            'password': 'password123',
+            'password_confirm': 'password123'
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'account/register.html')
+
+    def test_dashboard_requires_login(self):
+        response = self.client.get(self.dashboard_url)
+        self.assertEqual(response.status_code, 302)
+
+    def test_user_settings_authenticated_access(self):
+        self.client.login(username='testuser', password='password123')
+        response = self.client.get(self.settings_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'account/settings.html')
--- note/tests.py
+++ note/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from note.models import Note, Course, University, Favourite
 
-# Create your tests here.
+class NoteViewsTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='password123')
+        self.university = University.objects.create(name='Test University')
+        self.course = Course.objects.create(name='Test Course', university=self.university)
+        self.note = Note.objects.create(title='Test Note', body='Some content', author=self.user, course=self.course)
+
+    def test_note_list_view(self):
+        self.client.login(username='testuser', password='password123')
+        response = self.client.get(reverse('note:note_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'notes/notes_all.html')
+
+    def test_note_detail_view(self):
+        response = self.client.get(reverse('note:note_detail', args=[self.note.id, self.note.slug]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'notes/note_detail.html')
+
+    def test_add_favorite_note(self):
+        self.client.login(username='testuser', password='password123')
+        response = self.client.post(reverse('note:toggle_favorite', args=[self.note.id]))
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(Favourite.objects.filter(user=self.user, note=self.note).exists())
+
+    def test_course_list_view(self):
+        self.client.login(username='testuser', password='password123')
+        response = self.client.get(reverse('note:course_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'courses/courses_all.html')
